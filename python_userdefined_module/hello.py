7a872ee7da74af5a75662f1b7627caf836e699a7
Suraj Prakas Panday <PRAKAS10BCA@GMAIL.COM>
2026-09-16 21:48:26 +0530

    Add user-defined module with basic arithmetic operations and greeting function
    - Created hello.py with functions for greeting, addition, subtraction, multiplication, and division.
    - Implemented hi.py to demonstrate usage of the functions from hello.py.

---
+++ python_userdefined_module/hello.py
@@ -0,0 +1,19 @@
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def add_numbers(x, y):
+    return x + y
+
+def subtract_numbers(x, y):
+    return x - y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def divide_numbers(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
+    return x / y   
+
---
+++ python_userdefined_module/hi.py
@@ -0,0 +1,9 @@
+import hello
+
+a=4
+b=6
+
+print(hello.add_numbers(a,b))
+print(hello.subtract_numbers(a,b))
+print(hello.multiply_numbers(a,b))
+print(hello.divide_numbers(a,b))
